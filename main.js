39576a7da680470f4d3002654daced78b61d933e
alquimara <alquimara.a.braz@gmail.com>
2022-06-01 18:40:17 -0300

    adicionado as sections home, serviços e about; realizado as transições do menu e ancorando os sections

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home,
+#home img, 
+#home .stat h3,
+#home .stat p,
+#services, 
+#services header,
+#services .card,
+#about,
+#about h4,
+#about h2,
+#about .content`)
